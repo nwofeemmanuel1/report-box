712ddbf5dc48d244f829dee55f80d8750ba7e835
nwofeemmanuel1 <enwofe2020@gmail.com>
2022-03-17 08:02:13 +0100

    changes

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use("/api/user/register",registerUser)
 const login=require("./api/login")
 app.use("/api/user/login",login)
 
+app.get("/",(req,res)=>{
+    res.send("reportbox running...")
+})
+
 let port=process.env.PORT||3000
 app.listen(port,()=>console.log(`Server running on port ${port}`))
